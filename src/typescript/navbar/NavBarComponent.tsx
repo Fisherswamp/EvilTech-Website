1aefd8841b4904d821f3fb14bd7a46b2343308b3
Itai Fish <chessandpotatoes@gmail.com>
2020-09-20 15:01:51 -0400

    Adding navbar, working on main site

---
+++ src/typescript/navbar/NavBarComponent.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export interface NavBarComponentProps {
+	
+}
+ 
+export interface NavBarComponentState {
+	
+}
+ 
+class NavBarComponent extends React.Component<NavBarComponentProps, NavBarComponentState> {
+	constructor(props: NavBarComponentProps) {
+		super(props);
+		this.state = {   };
+	}
+	render() { 
+		return (
+			<Navbar className="darkprimary navheight" expand="lg" variant="dark" bg="dark">
+				<Navbar.Brand href="#home" className="titlefont darkprimary" >Evil Tech</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+  				<Navbar.Collapse id="basic-navbar-nav navbarColor02">
+					<Nav className="mr-auto">
+      					<Nav.Link href="#home" className="darkprimary titlefont-medium">Home</Nav.Link>
+						<Nav.Link href="#about" className="darkprimary titlefont-medium">About</Nav.Link>
+						<Nav.Link href="#about" className="darkprimary titlefont-medium">Portfolio</Nav.Link>
+						<Nav.Link href="#about" className="darkprimary titlefont-medium">Careers</Nav.Link>
+						<Nav.Link href="#contact" className="darkprimary titlefont-medium">Contact</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar> 
+		);
+	}
+}
+ 
+export default NavBarComponent;
